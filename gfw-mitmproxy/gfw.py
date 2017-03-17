348a4dfb158aad0d62d4a08464a832fe521a9775
tyage <namatyage@gmail.com>
2017-03-17 10:45:47 +0900

    big brother is watching you

--- gfw-mitmproxy/gfw.py
+++ gfw-mitmproxy/gfw.py
@@ -3,4 +3,4 @@ import pprint
 
 def response(flow):
     if '天安門事件'.encode('utf-8') in flow.response.content:
-        flow.response.content = '検閲済み'.encode('utf-8')
+        flow.response.content = '<img src="http://cdn-ak.f.st-hatena.com/images/fotolife/x/xshintarox/20131228/20131228164639.jpg">'.encode('utf-8')
